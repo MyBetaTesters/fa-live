70e91006d2ec361424f64b0c0be23299513a31ee
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-06-01 19:47:41 +0100

    using flask websockets

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 #!/bin/bash/python3
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_sockets import Sockets
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret!')
+sockets = Sockets(app)
 
-socketio = SocketIO(app)
-
-@socketio.on('position')
-def handle_message(message, *args, **kwargs):
+@sockets.route('/echo')
+def echo_socket(ws, *args, **kwargs):
+    message = ws.receive()
     print('msg recv:', message)
-    socketio.emit('position', message, broadcast=True)
+    ws.send(message)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
