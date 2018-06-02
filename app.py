2311f42a3c2226d11ba930700b6da6861978dc06
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-06-02 22:00:21 +0100

    working echo example

--- app.py
+++ app.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sockets import Sockets
-import os
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
 @sockets.route('/echo')
-def echo_socket(ws, *args, **kwargs):
-    message = ws.receive()
-    print('msg recv:', message)
-    ws.send(message)
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        print('recv: ', message)
+        ws.send(message)
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
