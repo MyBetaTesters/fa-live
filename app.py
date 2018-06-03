4120dffe4c6db6998337c673bdca6a1fd1fb0676
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-06-03 03:32:40 +0100

    chat app example from heroku

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
 #!/bin/bash/python3
 # -*- coding: utf-8 -*-
+import os
+import logging
+import redis
+import gevent
 from flask import Flask
 from flask_sockets import Sockets
 
+REDIS_URL = os.environ['REDIS_URL']
+REDIS_CHAN = 'adsb'
+
 app = Flask(__name__)
 sockets = Sockets(app)
+redis = redis.from_url(REDIS_URL)
+
+class AdsbReceiver(object):
+    def __init__(self):
+        self.clients = list()
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe(REDIS_CHAN)
+
+    def __iter_data(self):
+        for message in self.pubsub.listen():
+            data = message.get('data')
+            if message['type'] == 'message':
+                app.logger.info(u'Sending message: {}'.format(data))
+                yield data
+
+    def register(self, client):
+        self.clients.append(client)
+
+    def send(self, client, data):
+        try:
+            client.send(data)
+        except Exception:
+            self.clients.remove(client)
+
+    def run(self):
+        for data in self.__iter_data():
+            for client in self.clients:
+                gevent.spawn(self.send, client, data)
+
+    def start(self):
+        gevent.spawn(self.run)
+
+recvr = AdsbReceiver()
+recvr.start()
 
 @sockets.route('/echo')
 def echo_socket(ws):
@@ -12,6 +53,27 @@ def echo_socket(ws):
         message = ws.receive()
         print('recv: ', message)
         ws.send(message)
+
+@sockets.route('/tx')
+def inbox(ws):
+    while not ws.closed:
+        # Sleep to prevent *constant* context-switches.
+        gevent.sleep(0.1)
+        message = ws.receive()
+        print('tx: ', message)
+
+        if message:
+            app.logger.info(u'Inserting message: {}'.format(message))
+            redis.publish(REDIS_CHAN, message)
+
+@sockets.route('/rx')
+def outbox(ws):
+    print('connected')
+    recvr.register(ws)
+
+    while not ws.closed:
+        # Context switch while `ChatBackend.start` is running in the background.
+        gevent.sleep(0.1)
 
 @app.route('/')
 def hello():
